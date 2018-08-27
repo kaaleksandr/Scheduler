6f4d9af85ad45301360541ffefb19d835bbb8948
master-prog <master-prog@hotmail.com>
2018-08-28 01:44:03 +0300

    Init

---
+++ DaysPravoslavie.Parser/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysPravoslavie
+{
+    public class PravDay
+    {
+        /// <summary>
+        /// Конструктор.
+        /// </summary>
+        /// <param name="date">Дата.</param>
+        public PravDay(DateTime date)
+        {
+            Date = date;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime Date { get; private set; }
+    }
+}
